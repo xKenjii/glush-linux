8b7fc859d6b8659b0bacb50acde953db142efa1a
Kenjii <mariusvangriensven@gmail.com>
2022-02-09 23:06:36 +0100

    DDNS Implementation

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os.path, requests, json, requests, uuid
 if not os.path.isfile('./config.json'):
     print("Configuration file not found...")
     f = open('error-logs.txt', 'a')
-    f.write("\nconfig.json was not found...")
+    f.write("config.json was not found...\n")
     f.close()
     exit()
 
@@ -16,13 +16,13 @@ with open('config.json', 'r') as jsonfile:
 
 if config['apikey'] == "":
     f = open('error-logs.txt', 'a')
-    f.write("\nThe Api Key in config.json is unspecified.")
+    f.write("The Api Key in config.json is unspecified.\n")
     f.close()
     exit()
 
 if config['devicename'] == "":
     f = open('error-logs.txt', 'a')
-    f.write("\nThe Device Name in config.json is unspecified.")
+    f.write("The Device Name in config.json is unspecified.\n")
     f.close()
     exit()
 
@@ -31,11 +31,19 @@ if config['devicename'] == "":
     ############
 
 def pingServer():
-    r = requests.get('http://glush.cyberise.nl/api/sync.php?apikey=' + config['apikey'] + '&devicename=' + config['devicename'] + '&uniqueid=' + str(uuid.getnode())).json()
+
+    URL = 'http://ddns.defi-mining.nl/api/sync.php?uniqueid=' + str(uuid.getnode())
+    
+    for values in config.items():
+        URL += f"&{values[0]}={values[1]}"
+
+    print(URL)
+
+    r = requests.get(URL).json()
     
     if r['response'] != 'SUCCESS':
         f = open('error-logs.txt', 'a')
-        f.write("\nAn error occurred trying to synchronise this device with the Glush server:" + r['error'])
+        f.write(f"An error occurred trying to synchronise this device with the Glush server: {r['error']}\n")
         f.close()
 
 pingServer()
